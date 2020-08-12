99ff0b7efebcc0e7ccdd7903e9e10160683ab164
Joe Wilner <jhwilner1@gmail.com>
2020-08-12 09:32:30 -0400

    Deal with some boundary conditions

--- iter.go
+++ iter.go
@@ -1,6 +1,7 @@
 package ipx
 
 import (
+	"bytes"
 	"net"
 )
 
@@ -41,11 +42,14 @@ func (i *IPIter) IP() net.IP {
 func (i *IPIter) Next() bool {
 	if i.flags&ipIterFlagV6 > 0 {
 		if i.flags&ipIterFlagNegative > 0 {
-			if i.v6.val.Cmp(i.v6.limit) == -1 {
+			if i.v6.val.Cmp(i.v6.limit) != 1 {
 				return false
 			}
 			from128(i.v6.val, i.ip)
-			i.v6.val = i.v6.val.Minus(i.v6.incr)
+			old := i.v6.val
+			if i.v6.val = i.v6.val.Minus(i.v6.incr); old.Cmp(i.v6.val) == -1 {
+				i.v6.val = uint128{0, 0}
+			}
 			return true
 		}
 		if i.v6.val.Cmp(i.v6.limit) != -1 {
@@ -56,7 +60,7 @@ func (i *IPIter) Next() bool {
 		return true
 	}
 	if i.flags&ipIterFlagNegative > 0 {
-		if i.v4.val < i.v4.limit {
+		if i.v4.val <= i.v4.limit {
 			return false
 		}
 		from32(i.v4.val, i.ip)
@@ -80,59 +84,9 @@ func IterIP(start net.IP, step int, end net.IP) *IPIter {
 	}
 
 	if start.To4() != nil {
-		ip := to32(start)
-		if step > 0 {
-			limit := uint32(maxUint32)
-			if end != nil {
-				if end.To4() == nil {
-					return new(IPIter)
-				}
-				limit = to32(end)
-				if limit <= ip {
-					return new(IPIter)
-				}
-			}
-			return iterIPv4(ip, uint32(step), limit)
-		}
-		limit := uint32(0)
-		if end != nil {
-			if end.To4() != nil {
-				return new(IPIter)
-			}
-			limit = to32(end)
-			if limit >= ip {
-				return new(IPIter)
-			}
-		}
-		return iterIPv4(ip, uint32(step*-1), limit)
+		return resolveIPs4(start, step, end, 0)
 	}
-
-	ip := to128(start)
-	if step > 0 {
-		limit := uint128{maxUint64, maxUint64}
-		if end != nil {
-			if end.To4() == nil {
-				return new(IPIter)
-			}
-			limit = to128(end)
-			if limit.Cmp(ip) != 1 {
-				return new(IPIter)
-			}
-		}
-		return iterIPv6(ip, uint128{0, uint64(step)}, limit)
-	}
-
-	limit := uint128{}
-	if end != nil {
-		if end.To4() != nil {
-			return new(IPIter)
-		}
-		limit = to128(end)
-		if limit.Cmp(ip) != -1 {
-			return new(IPIter)
-		}
-	}
-	return iterIPv6(ip, uint128{0, uint64(step * -1)}, limit)
+	return resolveIPs6(start, step, end, 0)
 }
 
 func iterIPv4(val, incr, limit uint32) *IPIter {
@@ -176,41 +130,83 @@ func (n *NetIter) Next() bool {
 }
 
 // IterNet returns an iterator for the given increment starting with the provided network
-func IterNet(ipNet *net.IPNet, incr int) *NetIter {
-	mask := make(net.IPMask, len(ipNet.Mask))
-	copy(mask, ipNet.Mask)
+func IterNet(start *net.IPNet, step int, end *net.IPNet) *NetIter {
+	if step == 0 {
+		return new(NetIter)
+	}
+
+	var endIP net.IP
+	if end != nil {
+		if !bytes.Equal(start.Mask, end.Mask) {
+			return new(NetIter)
+		}
+		endIP = end.IP
+	}
+
+	mask := make(net.IPMask, len(start.Mask))
+	copy(mask, start.Mask)
 
 	ones, bits := mask.Size()
 	suffix := uint(bits - ones)
 
-	if ipNet.IP.To4() != nil {
-		if incr < 0 {
-			return &NetIter{
-				*iterIPv4(
-					to32(ipNet.IP),
-					uint32(incr*-1)<<suffix,
-					0,
-				),
-				&net.IPNet{Mask: mask},
+	if start.IP.To4() != nil {
+		return &NetIter{*resolveIPs4(start.IP, step, endIP, suffix), &net.IPNet{Mask: mask}}
+	}
+	return &NetIter{*resolveIPs6(start.IP, step, endIP, suffix), &net.IPNet{Mask: mask}}
+}
+
+func resolveIPs4(start net.IP, step int, end net.IP, shift uint) *IPIter {
+	sIP := to32(start)
+	if step > 0 {
+		eIP := uint32(maxUint32)
+		if end != nil {
+			if end.To4() == nil {
+				return new(IPIter)
+			}
+			eIP = to32(end)
+			if eIP <= sIP {
+				return new(IPIter)
 			}
 		}
-		return &NetIter{
-			*iterIPv4(
-				to32(ipNet.IP),
-				uint32(incr)<<suffix,
-				maxUint32,
-			),
-			&net.IPNet{Mask: mask},
+		return iterIPv4(sIP, uint32(step)<<shift, eIP)
+	}
+	var eIP uint32
+	if end != nil {
+		if end.To4() == nil {
+			return new(IPIter)
+		}
+		eIP = to32(end)
+		if eIP >= sIP {
+			return new(IPIter)
 		}
 	}
-	if incr < 0 {
-		return &NetIter{
-			*iterIPv6(to128(ipNet.IP), uint128{0, uint64(incr * -1)}.Lsh(suffix), uint128{}),
-			&net.IPNet{Mask: mask},
+	return iterIPv4(sIP, uint32(step*-1)<<shift, eIP)
+}
+
+func resolveIPs6(start net.IP, step int, end net.IP, shift uint) *IPIter {
+	sIP := to128(start)
+	if step > 0 {
+		eIP := uint128{maxUint64, maxUint64}
+		if end != nil {
+			if end.To4() != nil {
+				return new(IPIter)
+			}
+			eIP = to128(end)
+			if eIP.Cmp(sIP) != 1 {
+				return new(IPIter)
+			}
+		}
+		return iterIPv6(sIP, uint128{0, uint64(step)}.Lsh(shift), eIP)
+	}
+	var eIP uint128
+	if end != nil {
+		if end.To4() != nil {
+			return new(IPIter)
+		}
+		eIP = to128(end)
+		if eIP.Cmp(sIP) != -1 {
+			return new(IPIter)
 		}
 	}
-	return &NetIter{
-		*iterIPv6(to128(ipNet.IP), uint128{0, uint64(incr)}.Lsh(suffix), uint128{maxUint64, maxUint64}),
-		&net.IPNet{Mask: mask},
-	}
+	return iterIPv6(sIP, uint128{0, uint64(step * -1)}.Lsh(shift), eIP)
 }
--- iter_test.go
+++ iter_test.go
@@ -7,6 +7,55 @@ import (
 	"testing"
 )
 
+func TestIterIP(t *testing.T) {
+	for _, c := range []struct {
+		name     string
+		start    string
+		step     int
+		end      string
+		expected []string
+	}{
+		{"ipv4 incr", "10.0.0.0", 1, "10.0.0.3", []string{"10.0.0.0", "10.0.0.1", "10.0.0.2"}},
+		{"ipv4 incr equal", "10.0.0.0", 1, "10.0.0.0", []string{}},
+		{"ipv4 incr end exclusive", "10.0.0.0", 2, "10.0.0.2", []string{"10.0.0.0"}},
+		{"ipv4 incr end awkward", "10.0.0.0", 2, "10.0.0.3", []string{"10.0.0.0", "10.0.0.2"}},
+
+		{"ipv4 decr", "10.0.0.3", -1, "10.0.0.0", []string{"10.0.0.3", "10.0.0.2", "10.0.0.1"}},
+		{"ipv4 decr equal", "10.0.0.0", -1, "10.0.0.0", []string{}},
+		{"ipv4 decr end exclusive", "10.0.0.3", -2, "10.0.0.1", []string{"10.0.0.3"}},
+		{"ipv4 decr end awkward", "10.0.0.3", -2, "10.0.0.0", []string{"10.0.0.3", "10.0.0.1"}},
+
+		{"ipv6 incr", "::", 1, "::3", []string{"::", "::1", "::2"}},
+		{"ipv6 incr equal", "::", 1, "::", []string{}},
+		{"ipv6 incr end exclusive", "::", 2, "::2", []string{"::"}},
+		{"ipv6 incr end awkward", "::", 2, "::3", []string{"::", "::2"}},
+
+		{"ipv6 decr", "::3", -1, "::", []string{"::3", "::2", "::1"}},
+		{"ipv6 decr equal", "::", -1, "::", []string{}},
+		{"ipv6 decr end exclusive", "::3", -2, "::1", []string{"::3"}},
+		{"ipv6 decr end awkward", "::3", -2, "::", []string{"::3", "::1"}},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			//if !strings.HasSuffix(t.Name(), "ipv4_decr_end_awkward") {
+			//	t.SkipNow()
+			//}
+			start, end := net.ParseIP(c.start), net.ParseIP(c.end)
+			var got []string
+			for ips := ipx.IterIP(start, c.step, end); ips.Next(); {
+				got = append(got, ips.IP().String())
+			}
+			if len(got) != len(c.expected) {
+				t.Fatalf("Expected %v items but got %v: %v", len(c.expected), len(got), got)
+			}
+			for i := range got {
+				if c.expected[i] != got[i] {
+					t.Errorf("expected %v at position %v but got %v", c.expected[i], i, got[i])
+				}
+			}
+		})
+	}
+}
+
 func ExampleIterIP() {
 	ip := net.ParseIP("10.0.0.0")
 	for i, iter := 0, ipx.IterIP(ip, 100, nil); i < 5 && iter.Next(); i++ {
@@ -30,7 +79,7 @@ func ExampleIterIP() {
 }
 
 func ExampleIterIP_IP6() {
-	ip := net.ParseIP("::")
+	ip := net.ParseIP("2001:db8::")
 	for i, iter := 0, ipx.IterIP(ip, 1e18, nil); i < 5 && iter.Next(); i++ {
 		ip = iter.IP()
 		fmt.Println(ip)
@@ -39,16 +88,16 @@ func ExampleIterIP_IP6() {
 		fmt.Println(iter.IP())
 	}
 	// Output:
-	// ::
-	// ::de0:b6b3:a764:0
-	// ::1bc1:6d67:4ec8:0
-	// ::29a2:241a:f62c:0
-	// ::3782:dace:9d90:0
-	// ::3782:dace:9d90:0
-	// ::29a2:241a:f62c:0
-	// ::1bc1:6d67:4ec8:0
-	// ::de0:b6b3:a764:0
-	// ::
+	// 2001:db8::
+	// 2001:db8::de0:b6b3:a764:0
+	// 2001:db8::1bc1:6d67:4ec8:0
+	// 2001:db8::29a2:241a:f62c:0
+	// 2001:db8::3782:dace:9d90:0
+	// 2001:db8::3782:dace:9d90:0
+	// 2001:db8::29a2:241a:f62c:0
+	// 2001:db8::1bc1:6d67:4ec8:0
+	// 2001:db8::de0:b6b3:a764:0
+	// 2001:db8::
 }
 
 func BenchmarkIterIP(b *testing.B) {
@@ -93,11 +142,11 @@ func BenchmarkIterIP(b *testing.B) {
 
 func ExampleIterNet() {
 	ipN := cidr("10.0.0.0/16")
-	for i, iter := 0, ipx.IterNet(ipN, 100); i < 5 && iter.Next(); i++ {
+	for i, iter := 0, ipx.IterNet(ipN, 100, nil); i < 5 && iter.Next(); i++ {
 		ipN = iter.Net()
 		fmt.Println(ipN)
 	}
-	for i, iter := 0, ipx.IterNet(ipN, -100); i < 5 && iter.Next(); i++ {
+	for i, iter := 0, ipx.IterNet(ipN, -100, nil); i < 5 && iter.Next(); i++ {
 		fmt.Println(iter.Net())
 	}
 	// Output:
@@ -114,25 +163,25 @@ func ExampleIterNet() {
 }
 
 func ExampleIterNet_IP6() {
-	ipN := cidr("::/64")
-	for i, iter := 0, ipx.IterNet(ipN, 1e18); i < 5 && iter.Next(); i++ {
+	ipN := cidr("2001:db8::/64")
+	for i, iter := 0, ipx.IterNet(ipN, 1e18, nil); i < 5 && iter.Next(); i++ {
 		ipN = iter.Net()
 		fmt.Println(ipN)
 	}
-	for i, iter := 0, ipx.IterNet(ipN, -1e18); i < 5 && iter.Next(); i++ {
+	for i, iter := 0, ipx.IterNet(ipN, -1e18, nil); i < 5 && iter.Next(); i++ {
 		fmt.Println(iter.Net())
 	}
 	// Output:
-	// ::/64
-	// de0:b6b3:a764::/64
-	// 1bc1:6d67:4ec8::/64
-	// 29a2:241a:f62c::/64
-	// 3782:dace:9d90::/64
-	// 3782:dace:9d90::/64
-	// 29a2:241a:f62c::/64
-	// 1bc1:6d67:4ec8::/64
-	// de0:b6b3:a764::/64
-	// ::/64
+	// 2001:db8::/64
+	// 2de1:c46b:a764::/64
+	// 3bc2:7b1f:4ec8::/64
+	// 49a3:31d2:f62c::/64
+	// 5783:e886:9d90::/64
+	// 5783:e886:9d90::/64
+	// 49a3:31d2:f62c::/64
+	// 3bc2:7b1f:4ec8::/64
+	// 2de1:c46b:a764::/64
+	// 2001:db8::/64
 }
 
 func BenchmarkIterNet(b *testing.B) {
@@ -166,10 +215,10 @@ func BenchmarkIterNet(b *testing.B) {
 				b.Run(fmt.Sprintf("%v-%v", ones, c.incr), func(b *testing.B) {
 					b.ReportAllocs()
 
-					iter := ipx.IterNet(ipN, c.incr)
+					iter := ipx.IterNet(ipN, c.incr, nil)
 					for i := 0; i < b.N; i++ {
 						if !iter.Next() {
-							iter = ipx.IterNet(ipN, c.incr)
+							iter = ipx.IterNet(ipN, c.incr, nil)
 						}
 					}
 				})
